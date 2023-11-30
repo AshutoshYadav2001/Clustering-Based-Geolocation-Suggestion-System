aa0ec5f29c118468a3ff3956b3be9a64baa015e6
Ashutosh Yadav <ytosh0701@gmail.com>
2023-11-30 15:26:06 +0530

    importing required packages

---
+++ major.py
@@ -0,0 +1,15 @@
+import requests # library to handle requests
+import pandas as pd # library for data analsysis
+import numpy as np # library to handle data in a vectorized manner
+import random # library for random number generation
+import geopandas as gpd
+import matplotlib.cm as cm
+import folium
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import seaborn as sns
+from IPython.display import Image
+from IPython.core.display import HTML
+# tranforming json file into a pandas dataframe library
+from pandas.io.json import json_normalize
+print("All packages imported!")
