a64299acaa055a2014bd8d20e90778ac266c2fa7
Ashutosh Yadav <ytosh0701@gmail.com>
2023-12-06 13:25:13 +0530

    Create major.py

---
+++ major.py
@@ -0,0 +1,15 @@
+import requests # library to handle requests\n
+import pandas as pd # library for data analsysis\n
+import numpy as np # library to handle data in a vectorized manner\n
+import random # library for random number generation\n
+import geopandas as gpd\n
+import matplotlib.cm as cm\n
+import folium\n
+from sklearn.cluster import KMeans\n
+import matplotlib.pyplot as plt\n
+import seaborn as sns\n
+from IPython.display import Image\n
+from IPython.core.display import HTML\n
+# tranforming json file into a pandas dataframe library\n
+from pandas.io.json import json_normalize\n
+print(\"All packages imported!\")\n
